0dc56855479ff7fedc39b5b8fa281dfd8bc83b61
larsyaeger <larsyaeger@gmail.com>
2023-03-06 09:00:42 -0800

    starting to implement getStaticPaths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,17 +1,40 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import coffeeStoresData from "../../data/coffee-stores.json";
 
-const CoffeeStore = () => {
+export function getStaticProps({ params }) {
+  console.log("params here", params);
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id; //dynamic coffee store id
+      }),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: "0" } },
+      { params: { id: "1" } },
+      { params: { id: "300" } },
+    ],
+    fallback: false,
+  };
+}
+
+const CoffeeStore = (props) => {
+  console.log("props:", props);
   const router = useRouter();
-  console.log('router', router.query);
+  console.log("router", router.query);
   return (
-    <div>Coffee Store Page {router.query.id}
-    <Link href="/">
-      back to home
-      </Link>
-      <Link href="/coffee-store/one">
-      go to page dynamic
-      </Link>
+    <div>
+      Coffee Store Page {router.query.id}
+      <Link href="/">back to home</Link>
+      <Link href="/coffee-store/one">go to page dynamic</Link>
+      <p>{props.coffeeStore.address}</p>
+      <p>{props.coffeeStore.name}</p>
     </div>
   );
 };
