ac4b7f59a84ba4db695110b104a76a9e3be7ae52
larsyaeger <larsyaeger@gmail.com>
2023-03-08 18:31:39 -0800

    refactoring and moving function to a new file to clean up code

--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,13 @@ import Banner from "@/components/banner";
 import Card from "@/components/card";
 import Image from "next/image";
 import coffeeStoresData from "../data/coffee-stores.json";
+import { fetchCoffeeStores } from "@/lib/coffee-stores";
 
 export async function getStaticProps(context) {
-  const options = {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      Authorization: process.env.FOURSQUARE_API_KEY,
-    },
-  };
-
-  const response = await fetch(
-    "https://api.foursquare.com/v3/places/search?query=coffee&ll=43.653833032607096%2C-79.37896808855945&limit=6",
-    options
-  );
-  const data = await response.json();
-
-  // .catch((err) => console.error(err));
+  const coffeeStores = await fetchCoffeeStores();
   return {
     props: {
-      coffeeStores: data.results,
+      coffeeStores,
     },
   };
 }
