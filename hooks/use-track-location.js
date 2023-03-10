7419f83461d7954af039884a92b091835e173b25
larsyaeger <larsyaeger@gmail.com>
2023-03-10 15:35:56 -0800

    isFindingLocation functioning decently

--- hooks/use-track-location.js
+++ hooks/use-track-location.js
@@ -3,18 +3,23 @@ import { useState } from "react";
 export default function useTrackLocation() {
   const [locationErrorMsg, setLocationErrorMsg] = useState("");
   const [latLong, setLatLong] = useState("");
+  const [isFindingLocation, setIsFindingLocation] = useState(false);
   const success = (position) => {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     setLatLong(`${latitude},${longitude}`);
     setLocationErrorMsg("");
+    setIsFindingLocation(false);
   };
   const error = () => {
+    setIsFindingLocation(false);
     setLocationErrorMsg("Unable to retreive your location");
   };
   const handleTrackLocation = () => {
+    setIsFindingLocation(true);
     if (!navigator.geolocation) {
       setLocationErrorMsg("geolocation not supported by your browser");
+      setIsFindingLocation(false);
     } else {
       //status.textContent = "Locating...";
       navigator.geolocation.getCurrentPosition(success, error);
@@ -24,5 +29,6 @@ export default function useTrackLocation() {
     latLong,
     handleTrackLocation,
     locationErrorMsg,
+    isFindingLocation,
   };
 }
