31f4e99788f04ed85d0a11533736f046007ae883
larsyaeger <larsyaeger@gmail.com>
2023-03-29 16:35:26 -0700

    adding some context

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
-import '@/styles/globals.css'
+import { createContext } from "react";
+import "@/styles/globals.css";
+
+const StoreContext = createContext();
+
+const StoreProvider = ({ children }) => {
+  const initialState = {
+    latlong: "",
+    coffeeStores: [],
+  };
+  return (
+    <StoreContext.Provider value={{ state: initialState }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
 
 export default function App({ Component, pageProps }) {
   return (
-    <div>
+    <StoreProvider>
       <Component {...pageProps} />
-    </div>)
+    </StoreProvider>
+  );
 }
